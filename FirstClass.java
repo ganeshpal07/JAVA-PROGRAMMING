47a184e14dcb0376a7e280d4f779f8de770d98e5
ganeshpal07 <ganeshpal1516@gmail.com>
2026-08-06 22:00:15 +0530

    learning java

---
+++ FirstClass.java
@@ -0,0 +1,11 @@
+class FirstClass{
+    public static void main(String[] args){
+        System.out.println("Crack Google");
+        System.out.println("*");
+        System.out.println("**");
+        System.out.println("***");
+        
+        
+    }
+} 
+
