5b9d4f7dd4315aca3a6e882e6969d3e23653ee0d
ganeshpal07 <ganeshpal1516@gmail.com>
2026-08-22 12:25:10 +0530

    condition statements

---
+++ Conditions.java
@@ -0,0 +1,67 @@
+import java.util.*;
+public class Conditions {
+    // public static void main(String[] args){
+    //     Scanner sc = new Scanner(System.in);
+    //     int age = sc.nextInt();
+    //     if (age > 18){
+    //         System.out.println("Adult");
+    //     } else {
+    //         System.out.println("Not Adult");
+
+    //         sc.close();
+    //     }
+    // }
+        //  public static void main(String[] args){
+        //     Scanner sc = new Scanner(System.in);
+        //     int a = sc.nextInt();
+        //     if (a % 2 == 0){
+        //         System.out.println("even");
+        //     } else {
+        //         System.out.println("odd");
+
+        //         sc.close();
+        //     }
+            
+
+        //  }
+        
+            //  public static void main(String[] args){
+            //     Scanner sc  = new Scanner(System.in);
+            //     int a = sc.nextInt();
+            //     int b = sc.nextInt();
+
+            //     if(a == b){
+            //         System.out.println("equal");
+            //     } else { 
+            //         if(a > b){
+            //             System.out.println("greater");
+            //         } else {
+            //             System.out.println("smaller");
+
+            //             sc.close();
+            //         }
+            //     }
+                
+            //  }
+
+
+
+            // now using else if ladder.
+            public static void main(String[] args){
+                Scanner sc = new Scanner(System.in);
+                int a = sc.nextInt();
+                int b = sc.nextInt();
+
+                if(a == b){
+                    System.out.println("equal");
+                } else if(a > b){
+                    System.out.println(" a is greater");
+                }
+                else {
+                    System.out.println(" a is lesser");
+                }
+                  sc.close();
+                
+
+            }
+}
