a2ce993e06180c3d13cc006a54436d05603d2331
ganeshpal07 <ganeshpal1516@gmail.com>
2026-08-27 16:37:31 +0530

    more patterns

--- Patterns.java
+++ Patterns.java
@@ -78,18 +78,62 @@ public class Patterns {
 
     // printing "half pyramid"
 
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int n = 4;
+    // public static void main(String[] args) {
+    //     Scanner sc = new Scanner(System.in);
+    //     int n = 4;
 
-        for(int i = 1; i<=n; i++) {
-            for(int j = 1; j<=i; j++) {
+    //     for(int i = 1; i<=n; i++) {
+    //         for(int j = 1; j<=i; j++) {
                 // yha j-- i tak chalegaa kyuki i matlab upper limit
                 // and upper limit ----> kitne stars print honge 
+    //             System.out.print("*");
+    //         }
+    //         System.out.println();
+    //     }
+    //     sc.close();
+    // }
+
+
+    // printing "inverted half pyramid".....
+
+    // public static void main(String[] args) {
+    //     Scanner sc = new Scanner(System.in);
+    //     int n = 4;
+
+    //     for( int i = n; i>=1; i--) {
+            // here i=n : 4 stars print se start hoga aur; 4 rows
+            // i>=1 : 4 stars se 1 stars tak print hoga;
+            // i-- : har line ke baad i ki value decrease hoti jaaegi..
+    //         for(int j = 1; j<=i; j++) {
+    //             System.out.print("*");
+    //         }
+    //         System.out.println();
+    //     }
+    //     sc.close();
+    // }
+
+
+    // printing "half pyramid: mirror angle"
+
+    public static void main(String[] args) {
+        Scanner sc =  new Scanner(System.in);
+        int n = 4;
+
+        // outer loop
+        for(int i = 1; i<=n; i++) {
+            // inner loop 1 : for printing space
+            for(int j = 1; j<=n-i; j++) {
+                System.out.print(" ");
+            }
+            // inner loop 2: for printing stars
+            for(int j = 1; j<=i; j++) {
                 System.out.print("*");
             }
             System.out.println();
         }
         sc.close();
+        
     }
+
+
 }
