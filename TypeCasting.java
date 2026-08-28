14234aab19a99582d2667d38b02af91f2e78a7e9
ganeshpal07 <ganeshpal1516@gmail.com>
2026-08-28 08:59:06 +0530

    typecasting and typeconversions

---
+++ TypeCasting.java
@@ -0,0 +1,20 @@
+/*
+typecasting not allowed in java ; it is done by us forcefully.
+----- typecasting: storing bigger values in shorter form having loss of data
+*/
+
+public class TypeCasting {
+    public static void main(String[] args) {
+        // float a = 25.7f;
+        // int b = (int)a;
+
+        // System.out.println(b);
+
+        char ch = 'a';
+        int number = ch;
+
+        System.out.println(number);
+    //  in java every character alphabet has an number associated to it.
+      
+    }    
+}
---
+++ TypeConversion.java
@@ -0,0 +1,23 @@
+public class TypeConversion {
+
+    /*
+    possible scenarios
+    byte->short->int->float->long->double
+    or
+    Destination type > source type 
+    */
+    // public static void main(String[] args) {
+    //     int a = 5;
+    //     long b = a;
+
+    //     System.out.println(b);
+//    }
+
+   // not possible
+//    public static void main(String[] args) {
+//     long a = 5;
+//     int b = a;
+//     System.out.println(b);
+//  this will provide error: this is a lossy type conversion
+//    }
+}
