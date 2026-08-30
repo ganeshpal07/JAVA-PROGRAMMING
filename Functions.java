f878d5d35a1ecc84b7ae2a6e34b821fdf137d154
ganeshpal07 <ganeshpal1516@gmail.com>
2026-08-30 14:27:03 +0530

    functions questions

--- Functions.java
+++ Functions.java
@@ -8,19 +8,81 @@
 import java.util.*;
 
 public class Functions {
-    public static void printMyName(String name) {
-        System.out.println(name);
-        return;  
-        // return: to exit from the function
+    // public static void printMyName(String name) {
+    //     System.out.println(name);
+    //     return;  
+    //     // return: to exit from the function
+    // }
+
+    // public static void main(String[] args) {
+    //     Scanner sc = new Scanner(System.in);
+    //     String name = sc.next();
+
+    //     printMyName(name); 
+    //     //  call kiya function ko...
+    //     sc.close();
+    // }
+    
+    
+    // make a function to add two numbers and return the sum.
+
+    // public static int calculateSum(int a, int b) {
+    //     int sum = a+b;
+    //     return sum;
+    // }
+
+    // public static void main(String[] args) {
+    //     Scanner sc = new Scanner(System.in);
+    //     int a = sc.nextInt();
+    //     int b = sc.nextInt();
+
+    //     int sum = calculateSum(a, b);
+    //     // called function
+    //     System.out.println("sum of 2 numbers is :" +sum);
+    //     sc.close();
+    // }
+
+
+    // make a function to multiply 2 numbers and return the product.
+
+    // public static int calculateProduct(int a, int b) {
+        
+    //     return a*b;
+    // }
+
+    // public static void main(String[] args) {
+    //     Scanner sc = new Scanner(System.in);
+    //     int a = sc.nextInt();
+    //     int b = sc.nextInt();
+
+        
+    //     // called function
+    //     System.out.println("product of 2 numbers is :" +calculateProduct(a, b));
+    //     sc.close();
+    // }
+
+
+    // to print factorial of a number..
+
+    public static void printFactorial(int n) {
+        int factorial = 1;
+
+        if(n < 0) {
+            System.out.println("invalid number");
+            return;
+        }
+        for(int i=n; i>=1; i--) {
+            factorial = factorial*i;
+        }
+        System.out.println(factorial);
+        return;
     }
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        String name = sc.next();
+        int n = sc.nextInt();
 
-        printMyName(name); 
-        //  call kiya function ko...
+        printFactorial(n);
         sc.close();
     }
-    
 }
