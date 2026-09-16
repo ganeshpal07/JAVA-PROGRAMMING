ee62070b83137e5bea5c432460f46667b9d9fb85
ganeshpal07 <ganeshpal1516@gmail.com>
2026-09-16 19:52:43 +0530

    strings

---
+++ StringExample.java
@@ -0,0 +1,29 @@
+// String: stores words, text, sentences and paragraphs.
+/*
+there are two ways of writing string in java.
+1. String str = "abcd"
+2. String str = new String("xyz")
+.. strings are immutable in nature means they cannot be changed after execution. 
+*/
+// import java.util.*;
+
+public class StringExample {
+    // public static void main(String[] args) {
+    //     Scanner sc = new Scanner(System.in);
+    //     String name;
+    //     // name = sc.next(); used to print only a single word.
+    //     name = sc.nextLine();
+    //     // used to print a whole sentence and paragraphs.
+    //     System.out.println(name);
+    //     sc.close();
+        
+    // }
+
+    public static void main(String[] args) {
+        String fullName = "Ganesh Pal";
+        System.out.println(fullName.length());
+        // used to calculate length of a string.
+    }
+
+    
+}
