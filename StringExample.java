a239ede762fbde8419399892a32e0be7d7e8813f
ganeshpal07 <ganeshpal1516@gmail.com>
2026-09-17 19:30:54 +0530

    comparisons in strings

--- StringExample.java
+++ StringExample.java
@@ -5,7 +5,7 @@ there are two ways of writing string in java.
 2. String str = new String("xyz")
 .. strings are immutable in nature means they cannot be changed after execution. 
 */
-import java.util.*;
+// import java.util.*;
 
 public class StringExample {
     // public static void main(String[] args) {
@@ -52,25 +52,45 @@ public class StringExample {
 
     // check if a string is palindrome.
 
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        String str;
-        str = sc.next();
+    // public static void main(String[] args) {
+    //     Scanner sc = new Scanner(System.in);
+    //     String str;
+    //     str = sc.next();
 
-        for(int i=0; i<str.length()/2; i++) {
-            // jab tak index half string se chhota h tab takk loop chalega.
-            int n = str.length();
-            if(str.charAt(i) != str.charAt(n-i-1)) {
-                // agar character str index is not equal to its opposite character then not a  palindrome.
-                System.out.println("not a palindrome");
-                // agr humm break use nhi karte aur loop ke inside hi print karte to jab tak charcter mid tak nhi 
-                // reach karta tab tak "not a palindrome" aur "is palindrome" print hota
-                break;
-            } 
-        }
-        System.out.println("is palindrome");
-        sc.close();
+    //     for(int i=0; i<str.length()/2; i++) {
+    //         // jab tak index half string se chhota h tab takk loop chalega.
+    //         int n = str.length();
+    //         if(str.charAt(i) != str.charAt(n-i-1)) {
+    //             // agar character str index is not equal to its opposite character then not a  palindrome.
+    //             System.out.println("not a palindrome");
+    //             // agr humm break use nhi karte aur loop ke inside hi print karte to jab tak charcter mid tak nhi 
+    //             // reach karta tab tak "not a palindrome" aur "is palindrome" print hota
+    //             break;
+    //         } 
+    //     }
+    //     System.out.println("is palindrome");
+    //     sc.close();
 
+    // }
+
+    // comparing strings
+
+    public static void main(String[] args){
+        String s1 = "ganesh";
+        String s2 = "ganesh";
+        String s3 = new String("ganesh");
+
+        // if(s1==s2){
+        //     System.out.println("equal"); equal
+        // } else {
+        //     System.out.println("not equal");
+        // }
+
+        // if(s1==s3){
+        //     System.out.println("equal");
+        // } else{
+        //     System.out.println("not equal");   not equal
+        // }
     }
 
     
