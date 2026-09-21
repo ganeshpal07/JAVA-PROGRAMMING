d65fc481840d1bdd11fef40904283b7e6616b5e5
ganeshpal07 <ganeshpal1516@gmail.com>
2026-09-21 19:29:43 +0530

    clear bits in range

--- BitManipulation.java
+++ BitManipulation.java
@@ -1,8 +1,16 @@
-import java.util.*;
+// import java.util.*;
 
 public class BitManipulation {
     // Operations:
     // 1. Get bit: bitmask(i<<1) then use AND(&) operator to original nnumber.
+
+    // Clear range of bits
+    public static int clearBitsinRange(int n, int i, int j){
+        int a = ((~0)<<(j+1));
+        int b = (1<<i)-1;
+        int bitmask = a|b;
+        return n & bitmask;
+    }
     public static void main(String[] args) {
         // int n=5;
         // // 0101
@@ -36,21 +44,24 @@ public class BitManipulation {
         // for 0: bitmask(i<<1)--> AND with NOT.
         // for 1: bitmask(i<<1)--> OR.
 
-        Scanner sc = new Scanner(System.in);
-        int oper = sc.nextInt();
-        int n =5;
-        int pos =1;
-        int bitMask = 1<<pos;
+        // Scanner sc = new Scanner(System.in);
+        // int oper = sc.nextInt();
+        // int n =5;
+        // int pos =1;
+        // int bitMask = 1<<pos;
 
-        if(oper==1){
-            int newNumber = bitMask|n;
-            System.out.println(newNumber);
-        } else {
-            int newBitMask = ~(bitMask);
-            int newNumber = newBitMask;
-            System.out.println(newNumber);
-        }
-        sc.close();
+        // if(oper==1){
+        //     int newNumber = bitMask|n;
+        //     System.out.println(newNumber);
+        // } else {
+        //     int newBitMask = ~(bitMask);
+        //     int newNumber = newBitMask;
+        //     System.out.println(newNumber);
+        // }
+        // sc.close();
+
+
+        System.out.println(clearBitsinRange(10, 2, 4));
     }
 
     
