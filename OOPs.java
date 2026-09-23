2c816741ebb4a5d84dcf16ecd87a93d8b2e275e8
ganeshpal07 <ganeshpal1516@gmail.com>
2026-09-23 15:37:26 +0530

    encapsulation

--- OOPs.java
+++ OOPs.java
@@ -6,21 +6,41 @@
  method defined in class.
  */
 
-public class OOPs {
-    public static void main(String[] args){
-        Student s1 = new Student();
-        // created object s1 student
+public class OOPs{
+//     public static void main(String[] args){
+//         Student s1 = new Student();
+//         // created object s1 student
 
-        s1.setName("Ganesh Pal");
-        System.out.println(s1.name);
-    }    
+//         s1.setName("Ganesh Pal");
+//         System.out.println(s1.name);
+//     }    
+// }
+
+// class Student{
+//     String name;
+//     int age;
+
+//     void setName(String Name){
+//         name = Name;
+//     }
+
+
+// getters and setters.....
+
+public static void main(String[] args){
+    Pen p1 = new Pen();
+    p1.setcolor("blue");
+    System.out.println(p1.getColor());
+
+}
 }
 
-class Student{
-    String name;
-    int age;
-
-    void setName(String Name){
-        name = Name;
+class Pen{
+    private String color;
+    String getColor(){
+        return this.color;
+    }
+    void setcolor(String newColor){
+        color = newColor;
     }
 }
